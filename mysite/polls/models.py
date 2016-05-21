83ff7bf52bcfea6e0cff11308522605d35937b0f
comon <364834547@qq.com>
2016-05-21 18:03:55 +0800

    part1 2nd modify

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,20 +1,27 @@
 from __future__ import unicode_literals
 
+import datetime
 from django.db import models
+from django.utils import timezone 
 
 # Create your models here.
 class Question(models.Model):
     def __str__(self):
         class_name = "This is " + self.question_text
         return class_name
+
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
     def __str__(self):
         class_name = "this is " + self.choice_text
         return class_name
+
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
